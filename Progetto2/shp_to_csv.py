6580f53b9a016e75ff9bf1b93adafcf3c0c17fbe
Nicola Zecchillo <nzecchillo@studenti.apuliadigitalmaker.it>
2025-01-04 19:58:20 +0100

    Modifiche minori per cambio directory

--- Progetto2/shp_to_csv.py
+++ Progetto2/shp_to_csv.py
@@ -1,14 +1,14 @@
 import os
 import geopandas as gpd 
 
-comuni = gpd.read_file('../dati/Limiti01012024_g/Com01012024_g/Com01012024_g_WGS84.shp')
-comuni.to_csv('../dati/comuni.csv', index=False)
+comuni = gpd.read_file('Progetto2/input/Limiti01012024_g/Com01012024_g/Com01012024_g_WGS84.shp')
+comuni.to_csv('Progetto2/output/comuni.csv', index=False)
 
-provincie = gpd.read_file('../dati/Limiti01012024_g/ProvCM01012024_g/ProvCM01012024_g_WGS84.shp')
-provincie.to_csv('../dati/provincie.csv', index=False)
+provincie = gpd.read_file('Progetto2/input/Limiti01012024_g/ProvCM01012024_g/ProvCM01012024_g_WGS84.shp')
+provincie.to_csv('Progetto2/output/provincie.csv', index=False)
 
-regioni = gpd.read_file('../dati/Limiti01012024_g/Reg01012024_g/Reg01012024_g_WGS84.shp')
-regioni.to_csv('../dati/regioni.csv', index=False)
+regioni = gpd.read_file('Progetto2/input/Limiti01012024_g/Reg01012024_g/Reg01012024_g_WGS84.shp')
+regioni.to_csv('Progetto2/output/regioni.csv', index=False)
 
-ripartizioni = gpd.read_file('../dati/Limiti01012024_g/RipGeo01012024_g/RipGeo01012024_g_WGS84.shp')
-ripartizioni.to_csv('../dati/ripartizioni.csv', index=False)
+ripartizioni = gpd.read_file('Progetto2/input/Limiti01012024_g/RipGeo01012024_g/RipGeo01012024_g_WGS84.shp')
+ripartizioni.to_csv('Progetto2/output/ripartizioni.csv', index=False)
